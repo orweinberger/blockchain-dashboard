661033e34b7b323a65725d24b2541b5629d15744
orweinberger <orweinberger@gmail.com>
2014-12-10 23:33:30 +0200

    Minor improvements in tx pushing

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ var joola = require('./lib/joola');
 var async = require('async');
 
 var current_height = 0;
-var block_counter = 1;
-var tx_counter = 1;
-
+var block_counter = 0;
+var tx_counter = 0;
+/*
 function pushTransaction(height) {
   if (height <= current_height) {
     toshi.getBlock(height, function (data) {
@@ -14,7 +14,7 @@ function pushTransaction(height) {
           toshi.getTransaction(item, function (t) {
             delete t.inputs;
             delete t.outputs;
-            t.timestamp = t.block_time;
+            t.timestamp = new Date(t.block_time).toISOString();
             t.transaction_counter = 1;
             t.block_height = data.height;
             t.block_height_str = data.height.toString();
@@ -22,7 +22,6 @@ function pushTransaction(height) {
             t.amount_str = t.amount.toString();
             t.fees_str = t.fees.toString();
             t.confirmations_str = t.confirmations.toString();
-            t.push_timestamp = new Date().toISOString();
             return callback(null, t);
           });
         }, function (err, results) {
@@ -34,13 +33,63 @@ function pushTransaction(height) {
     });
   }
 }
+*/
+function pushTransaction(height) {
+  if (height != 0) {
+    toshi.getBlockTransactions(height, 1000, 0, function (data) {
+      var count = Math.ceil(data.transactions_count / 1000);
+      if (count === 1) {
+        async.map(data.transactions, function (t, callback) {
+          delete t.inputs;
+          delete t.outputs;
+          t.timestamp = new Date(t.block_time).toISOString();
+          t.transaction_counter = 1;
+          t.block_height = data.height;
+          t.block_height_str = data.height.toString();
+          t.size_str = t.size.toString();
+          t.amount_str = t.amount.toString();
+          t.fees_str = t.fees.toString();
+          t.confirmations_str = t.confirmations.toString();
+          return callback(null, t);
+        }, function (err, results) {
+          joola.pushDocument(results, 'transactions', function () {
+            console.log('pushed ' + results.length + ' txs for block', data.height);
+          });
+        });
+      }
+      else {
+        for (var i = 1; i <= count; i++) {
+          toshi.getBlockTransactions(height, 1000, 1000 * i - 1000, function (data) {
+            async.map(data.transactions, function (t, callback) {
+              delete t.inputs;
+              delete t.outputs;
+              t.timestamp = t.block_time;
+              t.transaction_counter = 1;
+              t.block_height = data.height;
+              t.block_height_str = data.height.toString();
+              t.size_str = t.size.toString();
+              t.amount_str = t.amount.toString();
+              t.fees_str = t.fees.toString();
+              t.confirmations_str = t.confirmations.toString();
+              t.push_timestamp = new Date().toISOString();
+              return callback(null, t);
+            }, function (err, results) {
+              joola.pushDocument(results, 'transactions', function () {
+                console.log('pushed ' + results.length + ' txs for block', data.height);
+              });
+            });
+          });
+        }
+      }
+    });
+  }
+}
 
 function pushBlock(height) {
   if (height <= current_height) {
     toshi.getBlock(height, function (data) {
       if (!data.error) {
-        var date = new Date(data.time);
-        data.timestamp = date.toISOString();
+        data.timestamp = new Date(data.time).toISOString();
         data.block_counter = 1;
         data.height_str = data.height.toString();
         data.fees_str = data.fees.toString();
@@ -49,7 +98,6 @@ function pushBlock(height) {
         data.reward_str = data.reward.toString();
         data.total_out_str = data.total_out.toString();
         data.transactions_count_str = data.transactions_count.toString();
-        data.push_timestamp = new Date().toISOString();
         delete data.next_blocks;
         delete data.transaction_hashes;
         joola.pushDocument(data, 'blocks', function () {
@@ -73,7 +121,7 @@ function addBlockTasks(num, interval, joola_height) {
   }
   async.series(tasks, function (err, result) {
     joola_height += num;
-    block_counter = 1;
+    block_counter = 0;
     addBlockTasks(num, interval, joola_height + 1);
   });
 }
@@ -91,7 +139,7 @@ function addTransactionsTask(num, interval, joola_height) {
   }
   async.series(tasks, function (err, result) {
     joola_height += num;
-    tx_counter = 1;
+    tx_counter = 0;
     addTransactionsTask(num, interval, joola_height + 1);
   });
 }
